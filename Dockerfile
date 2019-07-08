9ccfcb6dfbe42ddb63e3cc15cf25ee72d3c04f88
Timo Järventausta <timo.jarventausta@elisa.fi>
2019-07-08 20:27:49 +0300

    add interval config

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM alpine
 
 RUN apk update && apk add python mongodb-tools && mkdir /app
+ENV INTERVAL=daily
 
 WORKDIR /app
 RUN wget https://storage.googleapis.com/pub/gsutil.tar.gz && tar -xf gsutil.tar.gz
+
 COPY . ./
 RUN chmod +x *.sh
 
--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ echo gs_oauth2_refresh_token = $GS_TOKEN >> /root/.boto
 if [ -z "$RUN_ONCE" ]
 then
     mkfifo ./fifo
-    cp daily_backup.sh /etc/periodic/daily/
+    cp daily_backup.sh /etc/periodic/$INTERVAL/
     tail -f ./fifo
 else
     ./backup.sh
